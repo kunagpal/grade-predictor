a96f2c94c132072f90209ebed7e00e08a2ab1304
Kunal Nagpal <kunagpal@gmail.com>
2015-03-17 00:39:29 +0530

    Backend route construction work initiated.

--- app.js
+++ app.js
@@ -4,26 +4,30 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var csurf = require('csurf');
+var session = require('express-session');
+var routes = require(path.join(__dirname, 'routes', 'index'));
+var users = require(path.join(__dirname, 'routes', 'users'));
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.set('case sensitive routing', true);
+app.set('port', process.env.PORT || 3000);
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser('secret'));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(session({ secret : 'session secret key', resave : '', saveUninitialized : ''}));
+app.use(csurf());
 app.use('/', routes);
-app.use('/users', users);
+app.use('/', users);
+app.enable('trust proxy');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,61 @@
 var express = require('express');
 var router = express.Router();
-
+var temp;
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/grade', function(req, res){
+    if(req.signedCookies.name)
+    {
+        res.render('grade');
+    }
+    else
+    {
+        res.redirect('/login');
+    }
+});
+//GET login page
+router.get('/login', function(req, res){
+    if(req.signedCookies.name)
+    {
+        res.redirect('/');
+    }
+    else
+    {
+        temp = req.session.msg ? req.session.msg : 0;
+        delete req.session.msg;
+        res.render('login', {token : req.csrfToken(), msg : temp});
+    }
+});
+//GET signup page
+router.get('/signup', function(req, res){
+    temp = req.session.msg ? req.session.msg : 0;
+    delete req.session.msg;
+    res.render('signup', {token : req.csrfToken(), msg : temp});
+});
+//GET developers page
+router.get('/developers', function(req, res){
+    res.render('developers');
+});
+//GET forgot page
+router.get('/forgot', function(req, res){
+    res.render('forgot', {token : req.csrfToken()});
+});
+//GET logout page
+router.get('/logout', function(req, res){
+    if(req.signedCookies.name)
+    {
+        res.clearCookie('name');
+        res.redirect('/login');
+    }
+});
+// GET forgot password page
+router.get('/forgot', function(req, res) {
+    temp = req.session.msg ? req.session.msg : 0;
+    delete req.session.msg;
+    res.render('forgot', {token : req.csrfToken(), msg : temp});
+});
+// Generic get route request handler
+router.get(/\/.*/, function(req, res) {
+  res.render('index');
 });
 
 module.exports = router;
