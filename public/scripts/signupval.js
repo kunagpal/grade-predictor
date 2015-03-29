dd049a835ff8bf52618d02d4b238a78fa1e2eef9
bluffmaster95tanuj <bluffmaster.tanuj@gmail.com>
2015-03-29 14:59:01 +0530

    enlarging js added to signup

--- public/scripts/signupval.js
+++ public/scripts/signupval.js
@@ -10,6 +10,7 @@ function checkpswd() {
         window.alert("Password too short!");
     else if (l>20)
         window.alert("Password too long!");
+    document.getElementById("pwd").style.padding="15px";
 }
 function checkretype() {
     var pwd=document.getElementById("pwd").value;
@@ -19,4 +20,38 @@ function checkretype() {
         window.alert("Please re-type the password correctly!");
         document.getElementById("rpwd").value="";
     }
+    document.getElementById("rpwd").style.padding="15px";
+}
+
+function enlfname() {
+    document.getElementById("fname").style.padding = "25px";
+}
+function enlmname() {
+    document.getElementById("mname").style.padding = "25px";
+}
+function enllname() {
+    document.getElementById("lname").style.padding = "25px";
+}
+function enlrno() {
+    document.getElementById("rno").style.padding = "25px";
+}
+function enlpwd() {
+    document.getElementById("pwd").style.padding = "25px";
+}
+function enlrpwd() {
+    document.getElementById("rpwd").style.padding = "25px";
+}
+function enlemail() {
+    document.getElementById("email").style.padding = "25px";
+}
+function enldob() {
+    document.getElementById("dob").style.padding = "25px";
+}
+function small() {
+    document.getElementById("fname").style.padding = "15px";
+    document.getElementById("mname").style.padding = "15px";
+    document.getElementById("lname").style.padding = "15px";
+    document.getElementById("rno").style.padding = "15px";
+    document.getElementById("email").style.padding = "15px";
+    document.getElementById("dob").style.padding = "15px";
 }
