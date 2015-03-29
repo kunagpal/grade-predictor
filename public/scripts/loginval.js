16476563831905c16f13fd024dfd0be2318dbc3c
Vikramraj Sitpal <vikramrajsitpal@gmail.com>
2015-03-29 12:58:20 +0530

    added another

--- public/scripts/loginval.js
+++ public/scripts/loginval.js
@@ -4,4 +4,14 @@
 function checkid() {
     var i=document.getElementById("uid");
     uid.setAttribute("pattern" , "\d{2}\w{3}\d{4}");
+    document.getElementById("uid").style.padding = "15px";
+}
+function small() {
+    document.getElementById("pswd").style.padding = "15px";
+}
+function enlr() {
+    document.getElementById("uid").style.padding = "25px";
+}
+function enlp() {
+    document.getElementById("pswd").style.padding = "25px";
 }
