9221ba04a6514f17a2ba392461ace2a6649302e5
bluffmaster95tanuj <bluffmaster.tanuj@gmail.com>
2015-03-31 09:09:12 +0530

    developers page styled

---
+++ public/scripts/developval.js
@@ -0,0 +1,28 @@
+/**
+ * Created by TANUJ-pc
+
+*/
+
+
+
+
+
+function mouseOver() {
+    document.getElementById("img1").style.padding="0px";
+}
+function mouseOut() {
+    document.getElementById("img1").style.padding="10px";
+}
+
+function mouseOver1() {
+    document.getElementById("img2").style.padding="0px";
+}
+function mouseOut1() {
+    document.getElementById("img2").style.padding="10px";
+}
+function mouseOver2() {
+    document.getElementById("img3").style.padding="0px";
+}
+function mouseOut2() {
+    document.getElementById("img3").style.padding="10px";
+}
